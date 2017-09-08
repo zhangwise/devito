ef0c422c21d003ea15c5b370cd28681a9a211264
Michael Lange <michael.lange@imperial.ac.uk>
2017-09-08 10:54:02 +0100

    Acoustic: Add load/store for reference solution in convergence script

--- examples/seismic/acoustic/acoustic_example.py
+++ examples/seismic/acoustic/acoustic_example.py
@@ -128,11 +128,13 @@ def full(dimensions, spacing, tn, time_order, space_order, nbpml, **kwargs):
               help='Grid size of the reference solution')
 @click.option('--ref-order', type=int, default=40,
               help='Spatial order of the reference solution')
+@click.option('--ref-data', type=click.Choice(['exec', 'load', 'store']),
+              default='exec', help='What to do with reference solution data')
 @click.option('-s', '--scale', type=int, default=(1, ), multiple=True,
               help='Factor by which to scale the reference spacing/shape')
 @click.option('-o', '--order', type=int, default=(2, ), multiple=True,
               help='Spatial discretization order to run experiment')
-def convergence(dim, tn, ref_shape, ref_order, scale, order):
+def convergence(dim, tn, ref_shape, ref_order, ref_data, scale, order):
     """ Convergence test for an acoustic forward operator """
 
     # Reference run at with constant velocity and 1km grid in each dimension
@@ -157,13 +159,27 @@ def convergence(dim, tn, ref_shape, ref_order, scale, order):
     rec.coordinates.data[:, 0] = np.linspace(0., model.domain_size[0], num=nrec)
     rec.coordinates.data[:, 1:] = src.coordinates.data[0, 1:]
 
-    # Run reference model and store final wavefield
-    info("Running reference solution with shape %s order %d" %
-         (shape, ref_order))
-    _, u_r, _ = AcousticWaveSolver(model, source=src, receiver=rec,
-                                   space_order=ref_order).forward(save=False)
-    u_ref = u_r.data[tidx].copy()
+    # Generate/load/store the reference solution
+    fname = 'uref_dim%s_shape%s_order%s_tn%s' % (dim, ref_shape, ref_order, tn)
+    if ref_data in ['exec', 'store']:
+        # Run reference model and store store final wavefield
+        info('Computing reference solution with shape %s order %d' %
+             (shape, ref_order))
+        solver = AcousticWaveSolver(model, source=src, receiver=rec,
+                                    space_order=ref_order)
+        u_r = solver.forward(save=False)[1]
+        u_ref = u_r.data[tidx].copy()
 
+        if ref_data in ['store']:
+            info('Storing reference solution: %s' % fname)
+            np.save(fname, u_ref)
+
+    elif ref_data in ['load']:
+        info('Loading reference solution: %s.npy' % fname)
+        u_ref = np.load('%s.npy' % fname)
+        u_ref.reshape(shape)
+
+    # Perform benchmarks across parameter sets
     for o in order:
         for s in scale:
             info("Running experiement with order %d, scale %d" % (o, s))
@@ -184,7 +200,7 @@ def convergence(dim, tn, ref_shape, ref_order, scale, order):
                 u_ref.reshape(-1) / np.linalg.norm(u_ref.reshape(-1))
                 - u0.reshape(-1) / np.linalg.norm(u0.reshape(-1))
             )
-            info("Error[O::%d, S::%d] %f" % (o, s, error))
+            info("Error[O::%d, S::%d] %s" % (o, s, error))
 
 
 if __name__ == "__main__":
