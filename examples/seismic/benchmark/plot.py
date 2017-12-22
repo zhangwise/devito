839f4948fe650f8e60936fc0ef776ffcf76e4829
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-12-22 15:40:08 +0100

    Update plotting infrastructure

--- examples/seismic/benchmark/plot.py
+++ examples/seismic/benchmark/plot.py
@@ -1,50 +1,67 @@
-# python $DEVITO_HOME/examples/benchmark.py plot -P $problem -a -o -d $grid $grid $grid -so $space_orders -to $time_orders -r $DEVITO_RESULTS -p $DEVITO_PLOTS --max_bw 18.2 --max_flops 210
-
 import os
 import sys
 from subprocess import call
+import re
+
+#
+# Expected directory tree:
+#
+# sessionname (eg SISC-paper-2018; to be provided by user)
+#  | -- problemname (eg: acoustic, tti)
+#  |       | -- backend (eg: core, yask)
+#  |       |      | -- benchmode (eg: bmO2, bmdse)
+#  |       |      |       | -- archname (eg: knl7250)
+#  |       |      |       |       | -- grid512 -- files
+#  |       |      |       |       | -- grid768 -- files
+#  |       |      |       |       | -- grid1024 -- files
+#
+# E.g.: paper-SISC-2018/acoustic/core/bmO2/knl7250/grid512
+#
 
 if len(sys.argv) != 2:
-    print "Usage: python plot.py machine=[endeavour,hero]"
+    print("Usage: python plot.py absolute/path/to/session/name")
 
 machines = {}
-execfile('machines.txt', machines)
+exec(open("machines.txt").read(), {}, machines)
 
-devito_home = os.environ['DEVITO_HOME']
-results_dir = os.path.join(devito_home, '..', 'experimentation')
-raw_dir = [x[0] for x in os.walk(os.path.join(results_dir, 'raw', sys.argv[1]))]
-plots_dir = os.path.join(results_dir, 'plots', sys.argv[1])
+subfolders = ['problemname', 'backend', 'benchmode', 'archname', 'grid']
 
-for i in raw_dir[1:]:
-    location = i.split('/')[-1]
-    problem, mode, grid, arch, backend = location.split('-')
+benchmarkpy = os.path.join(os.environ['DEVITO_HOME'], 'examples', 'seismic',
+                           'benchmark', 'benchmark.py')
+
+root = sys.argv[1]
+dirs = [x[0] for x in os.walk(root)]
+
+root_depth = len(root.split('/'))
+plot_dirs = [i for i in dirs if len(i.split('/')) - len(subfolders) == root_depth]
+
+for i in plot_dirs:
+    problem, backend, benchmode, arch, _ = i.split('/')[root_depth:]
     files = [f for f in os.listdir(i) if f.endswith('json')]
     if not files:
-        print "WTF? "
+        print("WTF?")
         sys.exit(0)
-    sample = files[0]
 
     if arch not in machines:
-        print "Don't own any information for machine", arch
+        print("Don't own any information for %s architecture" % arch)
         sys.exit(0)
     machine = machines[arch]
 
-    grid = sample[sample.find("(")+1:sample.find(")")].split(',')[0]
-    space_orders = set(f[f.find('space_order')+11:].split('_')[0] for f in files)
-    space_orders = str(sorted([int(j) for j in list(space_orders)]))
-    space_orders = space_orders.replace(',', '').replace('[', '').replace(']', '')
-    tn = sample.split('tn')[1].split('.')[0]
+    grid = re.search(r"shape\[([A-Za-z0-9_,]+)\]", files[0]).group(1).split(',')
+    space_orders = sorted(set([int(re.search(r"so\[(\w+)\]", j).group(1)) for j in files]))
+    tn = re.search(r"tn\[(\w+)\]", files[0]).group(1)
 
-    if 'ekf' in arch:
-        os.environ['DEVITO_ARCH'] = 'knl'
-    else:
-        os.environ['DEVITO_ARCH'] = 'intel'
+    os.environ['DEVITO_ARCH'] = 'intel'
+    os.environ['DEVITO_BACKEND'] = backend
+    os.environ['DEVITO_AUTOTUNING'] = 'aggressive'
 
-    args = ['python', 'benchmark.py', '--bench-mode', mode, 'plot', '-P', problem, '-a', '-d',
-            grid, grid, grid, '-so', space_orders, '-to', '2', '--tn', tn,
-            '-r', i, '-p', plots_dir,
-            '--max_bw', str(machine['dram-stream-bw']),
-            '--max_flops', str(machine['machine-peak']),
-            '--point_runtime',
-            '--arch', arch]
+    args = ['python', benchmarkpy, 'plot', '-P', problem, '-bm', benchmode]  # plot cmd
+    args += ['-d'] + grid  # grid shape
+    for j in space_orders:
+        args += ['-so', str(j)]  # space orders tested
+    args += ['-to', '2', '--tn', tn]  # time-related simulation parameters
+    args += ['-r', i]  # plot dir
+    args += ['--arch', arch, '--max-bw', str(machine['dram-stream-bw']),  # arch params
+             '--flop-ceil', str(machine['machine-peak']), 'ideal',
+             '--flop-ceil', str(machine['linpack-peak']), 'linpack']
     call(' '.join(args), shell=True)
